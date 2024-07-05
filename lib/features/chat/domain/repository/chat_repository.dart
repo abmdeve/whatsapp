3e1c6c02a6c169897d645cbac82ffd234bbf5692
abmdeve <tundederome@gmail.com>
2024-07-05 17:02:46 +0200

    Update the code
    Implementation repository in domain directory

---
+++ lib/features/chat/domain/repository/chat_repository.dart
@@ -0,0 +1,14 @@
+import 'package:whatsapp/features/chat/domain/entities/chat_entity.dart';
+import 'package:whatsapp/features/chat/domain/entities/message_entity.dart';
+
+abstract class ChatRepository {
+  Future<void> sendMessage(ChatEntity chat, MessageEntity message);
+
+  Stream<List<ChatEntity>> getMyChat(ChatEntity chat);
+
+  Stream<List<MessageEntity>> getMessages(MessageEntity message);
+
+  Future<void> deleteMessage(MessageEntity message);
+
+  Future<void> deleteChat(ChatEntity chat);
+}
